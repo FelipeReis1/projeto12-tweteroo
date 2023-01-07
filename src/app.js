dc7590ffd6378dca46074aadfd0b9289c68f06f7
FelipeReis1 <feliperg15@yahoo.com.br>
2023-01-07 00:06:38 -0300

    requisito POST /tweets concluído

--- src/app.js
+++ src/app.js
@@ -12,9 +12,22 @@ const tweets = [];
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
   if (!username || !avatar) {
-    return res.status(400).send("Preencha todos os campos antes de enviar");
+    return res.status(422).send("Preencha todos os campos antes de enviar");
   }
   users.push({ username, avatar });
+  res.status(201).send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  if (!username || !tweet) {
+    return res.status(422).send("Preencha todos os campos antes de enviar");
+  }
+  const user = users.find((u) => u.username === username);
+  if (!user) {
+    return res.sendStatus(401);
+  }
+  tweets.push({ user, tweet });
   res.status(201).send("OK");
 });
 
